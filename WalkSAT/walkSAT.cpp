568149d0a616e84356794eae391baee23561c098
cwlui <cwlui@sfu.ca>
2020-08-08 20:38:09 -0700

    finished solve

--- WalkSAT/walkSAT.cpp
+++ WalkSAT/walkSAT.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <time.h>
+#include <cstdlib>
+#include <random>
 
 #include "walkSAT.h"
 
@@ -28,7 +31,70 @@ WalkSAT::~WalkSAT()
  */
 map<int, bool> WalkSAT::solve(int p, int max_flips)
 {
-    
+    srand(time(0)); // for rand set up the seed
+
+    map<int, bool> model;   // note: int cannot be 0
+    // give random asignments for all the symbols
+    for (int i = 1; i < numClauses + 1; i++) {
+        if (rand() % 1) {
+            model[i] = true;
+        }
+        else {
+            model[i] = false;
+        }
+    }
+
+    // Determine Unsat Clauses
+    for (int i = 0; i < numClauses; i++) {
+        if (!checkClause(allClauses[i], model)) {
+            unsatClauses.push_back(i); // note: i is the unique identifier for that clause
+        }
+    }
+    int numFlips = 0;
+
+    while (numFlips < max_flips) {
+        if (unsatClauses.size() == 0) {
+            return model;
+        }
+
+        // Choose a random clause from unsat list
+        vector<int> chosenClause = allClauses[rand() % unsatClauses.size()];
+
+        // https://stackoverflow.com/questions/5886987/true-or-false-output-based-on-a-probability
+        default_random_engine rand_engine;
+        uniform_real_distribution<> uniform_zero_to_one(0.0, 1.0);
+
+        // Randomly pick flipping algorithm with probability p
+        int chosenVariable;
+
+        if (uniform_zero_to_one(rand_engine) > p) {
+            // flip a random symbol in the clause
+            chosenVariable = chosenClause[rand() % chosenClause.size()];
+        }
+        else {
+            // flip the symbol that maximizes the number of sat clauses
+            int satClauseCount = 0; // initialize as the least number of possible satisfied clauses
+
+            // Loop through each variable in the clause and compare number of sat clauses
+            for (int i = 0; i < chosenClause.size(); i++) {
+                int tempVar = abs(chosenClause[i]);
+                int tempCount = satCount(model, tempVar);
+
+                if (tempCount > satClauseCount) {
+                    satClauseCount = tempCount;
+                    chosenVariable = tempVar;
+                }
+            }   
+        }
+
+        model[chosenVariable] = !model[chosenVariable];     // flip variable
+        numFlips++;
+    }
+
+    // no solution found
+    model.clear();
+    model[0] = false;
+    return model;
 }
 
 /* Displays the variable and assignment */
@@ -89,7 +155,7 @@ void WalkSAT::loadKB(char* filePath)
     numClauses = stoi(tokens[3]);
 
     // Go through the clauses
-    for (int i = 0; i < numClauses; i++) {
+    for (int i = 0; i < numClauses + 1; i++) {
         getline(in_file, newLine);
         tokens = split(newLine, ' ');
 
@@ -105,17 +171,31 @@ void WalkSAT::loadKB(char* filePath)
     in_file.close();
 }
 
-int WalkSAT::satCount(map<int, bool>& model, int symbol)
+/* Return the number of clauses satisfied by flipping the specified symbol*/
+int WalkSAT::satCount(map<int, bool> &model, int symbol)
 {
+    int count = 0;
 
+    model[symbol] = !model[symbol]; // flip the symbol
+    
+    // loop through all clauses and check if they are satisifed by the current model
+    for (auto it = allClauses.begin(); it != allClauses.end(); it++) {
+        if (checkClause(it->second, model)) {
+            count++;
+        }
+    }
+
+    model[symbol] = !model[symbol]; // flip the symbol back to original value
+    return count;
 }
 
-/* Check if the given clause is satisfied by the model */
+/* Check if the given clause is satisfied by the model 
+ * Returns True if clause is satisfied, else false */
 bool WalkSAT::checkClause(const vector<int>& clause, const map<int, bool>& model)
 {
     // Loop through all variables in the clause to check if any are true and match
     for (auto it = clause.begin(); it != clause.end(); it++) {
-        /* clause is true if:
+        /* clause is true (satisfied) if:
          * - *it is positive and the model for that variable is true OR
          * - *it is negative and the model for that varaible is false*/
         if ((*it > 0 && model.at(abs(*it))) || (*it > 0 && !(model.at(abs(*it))))) {
@@ -125,6 +205,7 @@ bool WalkSAT::checkClause(const vector<int>& clause, const map<int, bool>& model
     }
     return false;
 }
+
 /* Returns a vector of tokens from s that is split by the specified delimiter */
 vector<string> WalkSAT::split(const string &s, char delimiter) const
 {
@@ -145,4 +226,3 @@ vector<string> WalkSAT::split(const string &s, char delimiter) const
     }
     return tokens;
 }
-
--- WalkSAT/walkSAT.h
+++ WalkSAT/walkSAT.h
@@ -13,8 +13,8 @@ class WalkSAT {
         ~WalkSAT();
 
         map<int, bool> solve(int p, int max_flips);
-        void displayModel(map<int, bool>& model);
-        bool checkModel(const map<int, bool>& model);
+        void displayModel(map<int, bool> &model);
+        bool checkModel(const map<int, bool> &model);
     
     private:
         int numClauses;
@@ -23,8 +23,8 @@ class WalkSAT {
         vector<int> unsatClauses;           // the int uniquely idenfities each clause
 
         void loadKB(char* filePath);
-        int satCount(map<int, bool>& model, int symbol);
-        bool checkClause(const vector<int>& clause, const map<int, bool>& model);
+        int satCount(map<int, bool> &model, int symbol);
+        bool checkClause(const vector<int> &clause, const map<int, bool> &model);
 
         // Helper Functions
         vector<string> split(const string &s, char delimiter) const;
