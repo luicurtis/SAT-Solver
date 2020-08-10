ff4b7c9047a222adecf2430ffc1994fab4a4b219
cwlui <cwlui@sfu.ca>
2020-08-10 10:14:39 -0700

    removed functions that were not being used

--- walkSAT/python/walkSAT.py
+++ walkSAT/python/walkSAT.py
@@ -1,5 +1,6 @@
-''' Taken from aima-python repository 
-    Edited to isolate the WalkSAT algorihtm for comparing '''
+''' Source code from aima-python repository 
+    Edited to isolate the WalkSAT algorihtm for comparing 
+    and to accept CNF formatted clauses '''
 
 import random
 import sys
@@ -25,31 +26,6 @@ def pl_true(exp, model={}):
             return True
 
     return False
-
-def prop_symbols(x):
-    """Return the set of all propositional symbols in x."""
-    if not isinstance(x, Expr):
-        return set()
-    elif is_prop_symbol(x.op):
-        return {x}
-    else:
-        return {symbol for arg in x.args for symbol in prop_symbols(arg)}
-
-
-def is_prop_symbol(s):
-    """A proposition logic symbol is an initial-uppercase string.
-    >>> is_prop_symbol('exe')
-    False
-    """
-    return is_symbol(s) and s[0].isupper()
-
-def is_symbol(s):
-    """A string s is a symbol if it starts with an alphabetic char.
-    >>> is_symbol('R2D2')
-    True
-    """
-    return isinstance(s, str) and s[:1].isalpha()
-
 
 def WalkSAT(clauses, numClauses, numVariables, p=0.5, max_flips=10000):
     """Checks for satisfiability of all clauses by randomly flipping values of variables
